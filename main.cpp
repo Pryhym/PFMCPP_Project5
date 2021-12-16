2affdb3e8f9908ed7b6530af7abfeee66dca37e7
Pryhym <94821015+Pryhym@users.noreply.github.com>
2021-12-16 22:43:09 +0000

    task 1 - 9

--- main.cpp
+++ main.cpp
@@ -40,29 +40,310 @@ Purpose:  This project continues developing Project3.
  You can resolve any [-Wpadded] warnings by adding -Wno-padded to the list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
 
  */
-
+#include <iostream>
 /*
  copied UDT 1:
  */
+struct Laundromat
+{
+    int numWashers;
+    int numDryers; 
+    float amOfWaterUsedDaily {20.5f}; 
+    float amOfEnergyUsedDaily = 70.21f;
+    float amOfMoneyMadeDaily = 500.75f;
 
+    struct WashingMachine
+    {
+        std::string brand = "Maytag";
+        int modelNum;
+        float waterUsed;
+        float powUsed;
+        int washes;
+        WashingMachine(float water, float power, int wsh); 
+        void wash(); 
+        float countChange(int coins);
+        float downTime(float timeOff);
+    };
+    Laundromat(int wash, int dryers);
+    void waterShutOff();
+    void washClothes();
+    void dryClothes();
+    float makeChange(float price, float amPaid);  
+};
+
+Laundromat::Laundromat(int wash, int dryers) :
+numWashers (wash),
+numDryers (dryers)
+{
+    std::cout << "Laundromat Class" << std::endl;
+    std::cout << "constructing the Amount of Water Used is: " << amOfWaterUsedDaily << std::endl;
+}
+void Laundromat::washClothes()
+{
+    std::cout << "Number Of Dryers: " << numDryers << std::endl;
+    std::cout << std::endl;
+}
+
+void Laundromat::dryClothes()
+{
+
+}
+
+float Laundromat::makeChange(float price, float amPaid)
+{
+    return amPaid - price;
+}
+Laundromat::WashingMachine::WashingMachine(float water, float power, int wsh):
+waterUsed(water),
+powUsed(power),
+washes(wsh)
+{
+
+}
+void Laundromat::WashingMachine::wash()
+{
+
+}
+
+void Laundromat::waterShutOff()
+{
+    amOfWaterUsedDaily = 0;
+    while(amOfWaterUsedDaily < 200)
+    {
+        std::cout << "Amount Of Water Used: " << amOfWaterUsedDaily <<std::endl;
+        amOfWaterUsedDaily += 12.43f;
+    }
+    std::cout << std::endl;            
+    std::cout << "Water Exceeded Daily Amount!" << std::endl;
+    std::cout << std::endl;
+}
+
+float countChange(int coins)
+{
+    return coins;
+}
+
+float downTime(float timeOff)
+{
+    return timeOff;
+}
 /*
  copied UDT 2:
  */
+struct Restaurant
+{
+    int tblsAvail {9};
+    int workers = 8;
+    int platesServed {24};
+    float amFoodPerFamily = 79.48f;
+    float hoursWorked = 86.77f;
+    float amountPerTable;
+    Restaurant();
+
+    struct Server
+    {
+        std::string name = "James";
+        long phoneNum = 8434305954;
+        int amOfPeopleServed;
+        float hoursWorked;
+        Server():
+        amOfPeopleServed(8),
+        hoursWorked(17.5f)
+        {
+            std::cout << "Server Class Constructed!" << std::endl;
+        }
+        float currPay = 9.45f;
+        void serveCustomer();
+        void cleanTable();
+        float countTip(float tip);
+    };
+
+    void tableAmount(int platePerFamily, int families);
+    void serveFood();
+    float collectPayment(float payment);
+    void provideEatingUtensils();
+};
+
+Restaurant::Restaurant():
+workers()
+{
+    std::cout << "Restaurant Class" << std::endl;
+}
+
+void Restaurant::serveFood()
+{
+    
+}
+
+float Restaurant::collectPayment(float payment)
+{
+    return payment;
+}
+
+void Restaurant::provideEatingUtensils()
+{
+
+}
+
+void Restaurant::tableAmount(int platePerFamily, int families)
+{
+    while (families < platesServed) 
+    {
+        amountPerTable += (platePerFamily * amFoodPerFamily);
+        std::cout << "The Amount Of food served Total Is: " << amountPerTable << std::endl;
+        ++families;
+    }
+    std::cout << std::endl;
+}
+
+void Restaurant::Server::serveCustomer()
+{
+
+}
+
+void Restaurant::Server::cleanTable()
+{
+
+}
+
+float Restaurant::Server::countTip(float tip)
+{
+    return tip;
+}
 
 /*
  copied UDT 3:
  */
+struct Speakers
+{
+    std::string brand {"Vagnavox"};
+    int modelNum {214375};
+    int size = 12;
+    int time;
+    float freqRange = 15000.00f;
+    float impedence = 59.45f;
+    Speakers();
+    void produceAudio();
+    void receiveSignal();
+    void vibrateCone();
+};
 
+Speakers::Speakers():
+size(),
+freqRange()
+{
+    std::cout << "Speakers Class Created" << std::endl;
+}
+
+void Speakers::produceAudio()
+{
+    while (time < 15)
+    {
+        std::cout << "Producing Audio!!!!!!!" << std::endl;
+        ++time;
+    }
+    std::cout << std::endl;
+}
+
+void Speakers::receiveSignal()
+{
+    std::cout << "Model Number: " << modelNum << std::endl;
+}
+
+void Speakers::vibrateCone()
+{
+    
+}
 /*
  new UDT 4:
  with 2 member functions
  */
+struct Equipment
+{
+    Laundromat machine;
+    Laundromat::WashingMachine pipe;
+    Restaurant dishWasher;
+    Restaurant::Server carrier;
+    Speakers tweaters;
 
+    Equipment();
+    ~Equipment()
+    {
+        std::cout << "Equipment Class Destroyed!" << std::endl;
+    }
+
+    void laundromatEquipment();
+    void restaurantEquipment();
+    void speakerEquipment();
+};
+Equipment::Equipment():
+machine(20, 34),
+pipe(24.3f, 43.1f, 9),
+dishWasher(),
+carrier(),
+tweaters()
+{
+    
+    std::cout << "Equipment Class Created " << std::endl;
+}
+
+void Equipment::laundromatEquipment()
+{
+    std::cout << "Laundromat Machines in Inventory: " << machine.numWashers << std::endl;
+    std::cout << "Laundromat Dryers in Inventory: " << machine.numDryers << std::endl;
+}
+
+void Equipment::restaurantEquipment()
+{
+     std::cout << "Restaurant Workers: " << dishWasher.workers << std::endl;
+    std::cout << "Amount of Server Carriers In Inventory: " << carrier.amOfPeopleServed << std::endl;
+}
+
+void Equipment::speakerEquipment()
+{
+    std::cout << "Speaker Size: " << tweaters.size << std::endl;
+    std::cout << "Speaker Frequency Range: " << tweaters.freqRange << std::endl;
+}
 /*
  new UDT 5:
  with 2 member functions
  */
+struct Data
+{
+    Laundromat equipment;
+    Restaurant personal;
+    Speakers parts;
+    Laundromat::WashingMachine components;
 
+    Data();
+    ~Data()
+    {
+        std::cout << "Data Class Destroyed!" << std::endl;
+    }
+    
+    void laundromatData();
+    void restaurantData();
+};
+
+Data::Data():
+equipment(24, 32),
+personal(),
+parts(),
+components(624.54f, 215.34f, 12)
+{
+    std::cout << "Data Class Constructed!" << std::endl;
+}
+
+void Data::laundromatData()
+{
+    int equip = equipment.numWashers * equipment.numDryers;
+    std::cout << "Laundromat Equipment Amount: " << equip << std::endl;
+}
+
+void Data::restaurantData()
+{
+    std::cout << "Restraurant Personal working: " << personal.workers << std::endl;
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -80,5 +361,31 @@ Purpose:  This project continues developing Project3.
 #include <iostream>
 int main()
 {
+    Laundromat device(54, 40);
+    device.washClothes();
+    device.dryClothes();
+    std::cout << std::endl;
+
+    Restaurant food;
+    food.tableAmount(3,1);
+    std::cout << std::endl;
+
+    Speakers speak;
+    speak.receiveSignal();
+    speak.produceAudio();
+    speak.vibrateCone();
+    std::cout << std::endl;
+
+    Equipment hardware;
+    hardware.laundromatEquipment();
+    hardware.restaurantEquipment();
+    hardware.speakerEquipment();
+    std::cout << std::endl;
+
+    Data numbers;
+    numbers.laundromatData();
+    numbers.restaurantData();
+    std::cout << std::endl;
+
     std::cout << "good to go!" << std::endl;
 }
